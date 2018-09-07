18e85a8260b94d58c41908d18a3d08a1f2611503
Lee465357793 <465357793@qq.com>
2018-09-07 18:12:54 +0800

    上传颜色图片

--- app/src/main/java/com/lee/cameraorc/MainActivity.java
+++ app/src/main/java/com/lee/cameraorc/MainActivity.java
@@ -19,14 +19,17 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     private static final int REQUEST_A_CAMERA = 1000;
     private static final int REQUEST_B_CAMERA = 1001;
     private static final int REQUEST_C_CAMERA = 1002;
+    private static final int REQUEST_D_CAMERA = 1003;
     private static final String NAME_A = "photo_a.jpg";
     private static final String NAME_B = "photo_b.jpg";
     private static final String NAME_C = "photo_c.jpg";
+    private static final String NAME_D = "photo_d.jpg";
     private String mCurrFileName;
 
     private ImageView mVerifyPhotoA;
     private ImageView mVerifyPhotoB;
     private ImageView mVerifyPhotoC;
+    private ImageView mVerifyPhotoD;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -35,9 +38,11 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         findViewById(R.id.btn_general).setOnClickListener(this);
         findViewById(R.id.btn_card_front).setOnClickListener(this);
         findViewById(R.id.btn_card_back).setOnClickListener(this);
+        findViewById(R.id.btn_bank_card).setOnClickListener(this);
         mVerifyPhotoA = findViewById(R.id.image_a);
         mVerifyPhotoB = findViewById(R.id.image_b);
         mVerifyPhotoC = findViewById(R.id.image_c);
+        mVerifyPhotoD = findViewById(R.id.image_d);
     }
 
     @Override
@@ -53,6 +58,9 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                     filePath = new File(getFilesDir(), NAME_B).getAbsolutePath();
                     break;
                 case REQUEST_C_CAMERA: //身份证背面
+                    filePath = new File(getFilesDir(), NAME_C).getAbsolutePath();
+                    break;
+                case REQUEST_D_CAMERA: //银行卡
                     filePath = new File(getFilesDir(), NAME_C).getAbsolutePath();
                     break;
             }
@@ -78,6 +86,8 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
             mVerifyPhotoB.setImageBitmap(smallBitmap);
         } else if (NAME_C.equals(mCurrFileName)) {//身份证背面
             mVerifyPhotoC.setImageBitmap(smallBitmap);
+        } else if (NAME_D.equals(mCurrFileName)) {//身份证背面
+            mVerifyPhotoD.setImageBitmap(smallBitmap);
         }
     }
 
@@ -108,6 +118,14 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                         FileUtil.getSaveFile(getApplication(), NAME_C).getAbsolutePath());
                 intent.putExtra(CameraActivity.KEY_CONTENT_TYPE, CameraActivity.CONTENT_TYPE_ID_CARD_BACK);
                 startActivityForResult(intent, REQUEST_C_CAMERA);
+                break;
+            case R.id.btn_bank_card:
+                mCurrFileName = NAME_D;
+                intent = new Intent(this, CameraActivity.class);
+                intent.putExtra(CameraActivity.KEY_OUTPUT_FILE_PATH,
+                        FileUtil.getSaveFile(getApplication(), NAME_D).getAbsolutePath());
+                intent.putExtra(CameraActivity.KEY_CONTENT_TYPE, CameraActivity.CONTENT_TYPE_BANK_CARD);
+                startActivityForResult(intent, REQUEST_D_CAMERA);
                 break;
         }
 
